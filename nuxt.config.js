c2c422090ada1cae9b7a4cdede75ded982408680
mutarski <mutarski@gmail.com>
2023-05-21 01:19:53 +0200

    Change target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,23 @@
 export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  // ssr: false,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  // target: 'static',
+  target: 'static',
 
   // https://nuxt.com/docs/getting-started/seo-meta
   app: {
     head: {
-      title: 'road-wings',
+      title: 'Road Wings',
     },
+    meta: [
+      {
+        'http-equiv': 'Cache-Control',
+        content: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      },
+      { 'http-equiv': 'Pragma', content: 'no-cache' },
+      { 'http-equiv': 'Expires', content: '0' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
